972e149c5b17be18a0caafb5c83802fdc9b8072b
mauratxrealtor-max <mauratxrealtor@gmail.com>
2026-05-18 14:18:41 -0500

    Update fetch.py

--- scraper/fetch.py
+++ scraper/fetch.py
@@ -695,7 +695,7 @@ class ClerkScraper:
         cur = start
         while cur <= end:
             months.append((cur.year, cur.month))
-           if cur.month == 12:
+            if cur.month == 12:
                 cur = cur.replace(year=cur.year + 1, month=1)
             else:
                 cur = cur.replace(month=cur.month + 1)
